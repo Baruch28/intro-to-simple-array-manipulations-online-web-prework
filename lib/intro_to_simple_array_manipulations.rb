a2278ec7ed0fcefc5d5c6b4c1c543197effc5fb5
mark baruch <mbaruch28@gmail.com>
2019-02-06 20:08:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,3 @@
 def using_push
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  updated_array = using_push(countries_in_western_africa, next_country)
+  array.new
 end
