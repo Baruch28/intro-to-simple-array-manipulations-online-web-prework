daed3249b9ddc8a32534569498b56925a5d0a9e0
mark baruch <mbaruch28@gmail.com>
2019-02-06 19:39:22 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_push(array, "element")
+  element.push
+end
+
+def using_unshift(array)
+  array.unshift
+end
+
+def using_pop(array)
+  array.pop
+
+def pop_with_args(array)
+  array.pop_with_args
+
+def  using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift_with_args
+end
+
+def using_concat(array)
+  array.using_concat
+end
+
+def using_insert(array)
+  array.using_insert
+end
+
+def using_uniq(array)
+  array.using_uniq
+end
+
+def using_flatten(array)
+  array.using_flatten
+end
+
+def using_delete(array)
+  array.using_flatten
+end
+
+def using_delete_at(array, 3)
+  array.using_delete_at
+end
